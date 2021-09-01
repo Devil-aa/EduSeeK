03d87d6c2d57950dd4e2bc5bcca252555b19ee55
harsh2910 <harshshah29100@gmail.com>
2021-09-01 15:27:32 +0530

    Update views.py

--- intership/views.py
+++ intership/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User, Post
 
 # Create your views here.
 
 def interships(request):
-    return render(request, 'intership.html')
+    posts = Post.objects.order_by('-id')[:10]
+
+    return render(request, 'intership.html', {'posts': posts
+    })
 
