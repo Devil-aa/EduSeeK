09c8ca029086cadae2219f1e30c8e51740ea5aa3
Devil-aa <abhi9559137@gmail.com>
2021-09-01 14:45:16 +0530

    Update models.py

--- intership/models.py
+++ intership/models.py
@@ -5,10 +5,15 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    vacancy = models.CharField(max_length=255)
+    deadline = models.CharField(max_length=255)
     date = models.CharField(max_length=255)
     duration = models.CharField(max_length=255)
     stipend= models.CharField(max_length=255)
+    skills = models.CharField(max_length=255)
     last_date=models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
     
     def __str__(self):
         return self.title + ' | ' +str(self.author)
