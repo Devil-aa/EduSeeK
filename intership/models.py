97a0a55b0936638e0f46c947b59e7bb622b427e0
harsh2910 <harshshah29100@gmail.com>
2021-09-01 15:09:34 +0530

    location model update

--- intership/models.py
+++ intership/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Location(models.Model):
+    city = models.CharField(max_length=255)
+    code = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.city + ' | ' + self.code
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
@@ -13,14 +21,7 @@ class Post(models.Model):
     stipend= models.CharField(max_length=255)
     skills = models.CharField(max_length=255)
     last_date = models.CharField(max_length=255)
-    location = models.CharField(max_length=255)
+    location = models.ForeignKey(Location, blank=False, on_delete=models.CASCADE, related_name="intern")
     
     def __str__(self):
         return self.title + ' | ' + self.company
-
-class Location(models.Model):
-    city = models.CharField(max_length=255)
-    code = models.CharField(max_length=5)
-
-    def __str__(self):
-        return self.city + ' | ' + self.code
