8e2d2ac5aab9288e53aff0948f307daa2bebcf59
Devil-aa <abhi9559137@gmail.com>
2021-09-02 17:06:51 +0530

    .

--- intership/models.py
+++ intership/models.py
@@ -14,13 +14,13 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
     position = models.CharField(max_length=255)
-    vacancy = models.CharField(max_length=255)
-    deadline = models.CharField(max_length=255)
+    vacancy = models.IntegerField(default=0) 
+    deadline = models.DateField()
     date = models.CharField(max_length=255)
     duration = models.CharField(max_length=255)
-    stipend= models.CharField(max_length=255)
+    stipend= models.IntegerField(default=0) 
     skills = models.CharField(max_length=255)
-    content = models.CharField(max_length=255)
+    content = models.TextField(default="")
     location = models.ForeignKey(Location, blank=False, on_delete=models.CASCADE, related_name="intern")
     
     def __str__(self):
